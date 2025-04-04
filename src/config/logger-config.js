2083280e4006f9dbb51c0e8f19380290c1beb0a9
108aryanmaurya <108aryanmaurya@gmail.com>
2025-04-04 08:27:31 +0530

    Initial Commit

---
+++ src/config/logger-config.js
@@ -0,0 +1,20 @@
+const {format,transports,createLogger} =require('winston')
+const {combine,timestamp,label,printf}=format
+
+const customFormat=printf(({level,message,timestamp,label})=>{
+    return `${timestamp} : ${level} : ${message}`
+})
+
+const logger=createLogger({
+    format:combine(
+        timestamp({format:"YYYY-MM-DD HH:mm:ss"}),
+        customFormat
+    ),
+    transports:[
+        new transports.Console(),
+        new transports.File({filename:'combines.log'})
+    ]
+})
+
+
+module.exports=logger
---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express=require('express')
+const {ServerConfig} =require('./config') 
+const apiRoutes=require("./routes") 
+const app=express();
+
+app.use("/api",apiRoutes)
+
+app.listen(ServerConfig.PORT,()=>{
+    console.log(`Succesfully started the sever on PORT: ${ServerConfig.PORT} `)
+})
